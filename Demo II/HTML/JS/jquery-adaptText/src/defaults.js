13ecf42ad0f66dcd4a2601e000a419374c299229
Heila-Almogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-03-27 20:10:32 +0300

    removed unnecessary js files

--- Demo II/HTML/JS/jquery-adaptText/src/defaults.js
+++
@@ -1,9 +0,0 @@
-export default {
-  compression: 10,
-  max: Number.POSITIVE_INFINITY,
-  min: Number.NEGATIVE_INFINITY,
-  scrollable: false,
-  scrollSpeed: 1000,
-  scrollResetSpeed: 300,
-  onResizeEvent: true
-};
