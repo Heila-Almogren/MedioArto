c08fdcab73050419b94fb2ea0b3d785db1861182
Heila-Almogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-04-01 23:58:03 +0300

    changes

--- Demo II/HTML/JS/menuBar.js
+++ Demo II/HTML/JS/menuBar.js
@@ -5,29 +5,3 @@ $(document).ready(function() {
          $("#myForm").slideUp("fast");
          })
    });
-
-
-
-$(document).ready(function()
-{
-   $("#panel").hide();
-
-   $('.login').toggle(
-   function()
-   {
-      $('#panel').animate({
-      height: "150", 
-      padding:"20px 0",
-      backgroundColor:'#000000',
-      opacity:.8
-}, 500);
-   },
-   function()
-   {
-      $('#panel').animate({
-      height: "0", 
-      padding:"0px 0",
-      opacity:.2
-      }, 500);
-   });
-});
